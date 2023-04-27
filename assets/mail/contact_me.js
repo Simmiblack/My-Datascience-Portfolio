f45ffff5277a49ea9221b3d09d8c9833b36434f6
Simmiblack <31512409+Simmiblack@users.noreply.github.com>
2023-04-27 01:06:27 +0100

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://jia54crxfi.execute-api.eu-west-1.amazonaws.com/default/myDynamoDbLambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
